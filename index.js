1901d7dab4c0e41bfc8a2986a80130452f9bb826
Markus Lobedann <markus.lobedann@gmail.com>
2023-11-29 16:43:50 +0100

    neue Version

---
+++ index.js
@@ -0,0 +1,21 @@
+const currentVersionRules = "v0.6.0"
+
+window.onload = function () {
+    document.getElementById('current_version_rules').innerHTML = currentVersionRules;
+
+    const repoUrlRules = 'https://api.github.com/repos/Mutantpenguin/Universalis/releases/latest';
+
+    $.get(repoUrlRules, function (data) {
+        document.getElementById('beta_version_rules').innerHTML = data.tag_name;
+
+        if (data.tag_name === currentVersionRules) {
+            document.getElementById('alert_beta_version').classList.add("d-none");
+        }
+    });
+
+    // const repoUrlSoftware = 'https://api.github.com/repos/Mutantpenguin/UniversalisSoftware/releases/latest';
+
+    // $.get(repoUrlSoftware, function (data) {
+    //     document.getElementById('version_software').innerHTML = data.tag_name;
+    // });
+};
